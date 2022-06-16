169b675e02cee0aab739197388dec9f9b2958f47
ruudoolf <106280690+ruudoolf@users.noreply.github.com>
2022-06-16 10:10:37 +0200

    Loops

--- Assets/Scripts/FoodCanon.cs
+++ Assets/Scripts/FoodCanon.cs
@@ -5,26 +5,67 @@ using UnityEngine;
 public class FoodCanon : MonoBehaviour
 {
     [SerializeField] public GameObject[] food;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating(nameof(FireFood),0.5f, 0.5f);
+        InvokeRepeating(nameof(SpawnFoodDoWhile), 0.5f, 0.5f);
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void FireFood() 
-    { 
-    int randomFood = Random.Range(0,food.Length);
-    GameObject newfood = Instantiate(food[randomFood],transform.position, transform.rotation);
-        Rigidbody richtung = newfood.GetComponent<Rigidbody>();
-        richtung.AddForce(Random.insideUnitSphere*1000);
 
-    
+    }
+    private void FireFood()
+    {
+        int randomFood = Random.Range(0, food.Length);
+        GameObject newfood = Instantiate(food[randomFood], transform.position, transform.rotation);
+        Rigidbody direction = newfood.GetComponent<Rigidbody>();
+        direction.AddForce(Random.insideUnitSphere * 1000);
+    }
+
+    private void SpawnFoodForEach()
+    {
+        foreach (GameObject currentFood in food)
+        {
+            GameObject newfood = Instantiate(currentFood, transform.position, transform.rotation);
+            Rigidbody direction = newfood.GetComponent<Rigidbody>();
+            direction.AddForce(Random.insideUnitSphere * 1000);
+        }
+    }
+    private void SpawnFoodFor()
+    {
+        for (int i = 0; i < food.Length; i += 2)
+        {
+            GameObject newfood = Instantiate(food[i], transform.position, transform.rotation);
+            Rigidbody direction = newfood.GetComponent<Rigidbody>();
+            direction.AddForce(Random.insideUnitSphere * 1000);
+
+        }
+    }
+    private void SpawnFoodWhile()
+    {
+        int i = 0;
+        while (i < food.Length)
+        {
+            GameObject newfood = Instantiate(food[i], transform.position, transform.rotation);
+            Rigidbody direction = newfood.GetComponent<Rigidbody>();
+            direction.AddForce(Random.insideUnitSphere * 1000);
+            i++;
+        }
+    }
+    private void SpawnFoodDoWhile()
+    {
+        int i = 10;
+        do
+        {
+            GameObject newfood = Instantiate(food[0], transform.position, transform.rotation);
+            Rigidbody direction = newfood.GetComponent<Rigidbody>();
+            direction.AddForce(Random.insideUnitSphere * 1000);
+            i++;
+        }
+        while (i < food.Length);
     }
 }

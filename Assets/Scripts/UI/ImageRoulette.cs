d1fa9d1eb3d1c9ddf428ae819017035d2fcdd1d2
ruudoolf <106280690+ruudoolf@users.noreply.github.com>
2023-01-13 09:46:05 +0100

    Roulet

--- Assets/Scripts/UI/ImageRoulette.cs
+++ Assets/Scripts/UI/ImageRoulette.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageRoulette : MonoBehaviour
 {
@@ -10,49 +11,62 @@ public class ImageRoulette : MonoBehaviour
     [SerializeField] private GameObject levelThumbnail;
     [SerializeField] private float startSpeed = 10;
     [SerializeField] private float timeToStop = 5;
+    [SerializeField] private Sprite [] levelSprites;
+    
 
     private float currentSpeed;
     private float roundsUntilStop = 3;
-    private readonly List<GameObject> levelThumbnails = new List<GameObject>();
+    private readonly List<KeyValuePair<GameObject, bool>> levelThumbnails = new List<KeyValuePair<GameObject, bool>>();
+    private int winningNumber = 1;
 
-    private void SpawnThumbnail()
+    private void SpawnThumbnail(int thumbnail)
     {
-        GameObject newThumbnail = Instantiate(levelThumbnail, leftBoundary.transform.position, Quaternion.identity);
+        Vector3 distance = (rightBoundary.position - leftBoundary.position)/levelSprites.Length*thumbnail;
+        GameObject newThumbnail = Instantiate(levelThumbnail, leftBoundary.transform.position+distance, Quaternion.identity);
+        newThumbnail.GetComponent<Image>().sprite = levelSprites[thumbnail];
+        KeyValuePair<GameObject, bool> thumbnailWithWin = new KeyValuePair<GameObject, bool>(newThumbnail, thumbnail == winningNumber);
         newThumbnail.transform.SetParent(transform);
-        levelThumbnails.Add(newThumbnail);
+        levelThumbnails.Add(thumbnailWithWin);
     }
 
     private void Start()
     {
         currentSpeed = startSpeed;
-        SpawnThumbnail();
+        SpawnThumbnail(0);
+        SpawnThumbnail(1);
+        SpawnThumbnail(2);
     }
 
     private void Update()
     {
-        foreach (GameObject thumbnail in levelThumbnails)
+        foreach (KeyValuePair<GameObject, bool> thumbnail in levelThumbnails)
         {
-            thumbnail.transform.Translate(new Vector3(currentSpeed, 0, 0) * Time.deltaTime);
-
-            if (thumbnail.transform.position.x >= rightBoundary.position.x)
+            thumbnail.Key.transform.Translate(new Vector3(currentSpeed, 0, 0) * Time.deltaTime);
+            if (thumbnail.Key.transform.position.x >= rightBoundary.position.x)
             {
-                roundsUntilStop--;
-                thumbnail.transform.position = leftBoundary.position;
+                if (thumbnail.Value)
+                {
+                    roundsUntilStop--;
+                }
+                thumbnail.Key.transform.position = leftBoundary.position;
             }
-
-            if (roundsUntilStop > 0)
+            if (thumbnail.Value)
             {
-                float xDistance = rightBoundary.position.x - thumbnail.transform.position.x;
-                print(xDistance);
-                print(startSpeed + " || " + (startSpeed - startSpeed / roundsUntilStop));
-                currentSpeed = Mathf.Lerp(startSpeed, startSpeed - startSpeed / roundsUntilStop, 1f - xDistance);
+                if (roundsUntilStop > 0)
+                {
+                    float xDistance = rightBoundary.position.x - thumbnail.Key.transform.position.x;
+                    print(xDistance);
+                    print(startSpeed + " || " + (startSpeed - startSpeed / roundsUntilStop));
+                    currentSpeed = Mathf.Lerp(startSpeed, startSpeed - startSpeed / roundsUntilStop, 1f - xDistance);
+                }
+                else
+                {
+                    Vector3 centerPosition = (leftBoundary.position + rightBoundary.position) / 2;
+                    float xDistance = centerPosition.x - thumbnail.Key.transform.position.x;
+                    currentSpeed = Mathf.Lerp(startSpeed, 0, 1 - xDistance);
+                }
             }
-            else
-            {
-                Vector3 centerPosition = (leftBoundary.position + rightBoundary.position) / 2;
-                float xDistance = centerPosition.x - thumbnail.transform.position.x;
-                currentSpeed = Mathf.Lerp(startSpeed, 0, 1 - xDistance);
-            }
+           
         }
     }
 }

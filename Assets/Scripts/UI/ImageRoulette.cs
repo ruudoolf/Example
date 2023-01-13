cbe4f7b4f20b069fb47f096a58b771743c96394b
Daniel <logitin@pm.me>
2023-01-13 07:59:39 +0100

    Updated toon assets + working on roulette

--- Assets/Scripts/UI/ImageRoulette.cs
+++ Assets/Scripts/UI/ImageRoulette.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ImageRoulette : MonoBehaviour
@@ -7,43 +8,50 @@ public class ImageRoulette : MonoBehaviour
     [SerializeField] private RectTransform leftBoundary;
     [SerializeField] private RectTransform rightBoundary;
     [SerializeField] private GameObject levelThumbnail;
-    [SerializeField] private float speed = 10;
+    [SerializeField] private float startSpeed = 10;
     [SerializeField] private float timeToStop = 5;
 
-    [SerializeField] private bool startStopping = false;
-    private float stopTimer = 0;
-    private Vector3 centerPosition;
+    private float currentSpeed;
+    private float roundsUntilStop = 3;
     private readonly List<GameObject> levelThumbnails = new List<GameObject>();
 
     private void SpawnThumbnail()
     {
         GameObject newThumbnail = Instantiate(levelThumbnail, leftBoundary.transform.position, Quaternion.identity);
-        newThumbnail.transform.parent = transform;
+        newThumbnail.transform.SetParent(transform);
         levelThumbnails.Add(newThumbnail);
     }
 
     private void Start()
     {
-        centerPosition = (leftBoundary.position + rightBoundary.position) / 2;
+        currentSpeed = startSpeed;
         SpawnThumbnail();
     }
 
     private void Update()
     {
-        stopTimer += Time.deltaTime;
         foreach (GameObject thumbnail in levelThumbnails)
         {
-            thumbnail.transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+            thumbnail.transform.Translate(new Vector3(currentSpeed, 0, 0) * Time.deltaTime);
+
             if (thumbnail.transform.position.x >= rightBoundary.position.x)
             {
+                roundsUntilStop--;
                 thumbnail.transform.position = leftBoundary.position;
             }
-            //thumbnail.transform.position = Vector3.Lerp(thumbnail.transform.position, centerPosition, stopTimer / timeToStop);
 
-            if (startStopping)
+            if (roundsUntilStop > 0)
             {
+                float xDistance = rightBoundary.position.x - thumbnail.transform.position.x;
+                print(xDistance);
+                print(startSpeed + " || " + (startSpeed - startSpeed / roundsUntilStop));
+                currentSpeed = Mathf.Lerp(startSpeed, startSpeed - startSpeed / roundsUntilStop, 1f - xDistance);
+            }
+            else
+            {
+                Vector3 centerPosition = (leftBoundary.position + rightBoundary.position) / 2;
                 float xDistance = centerPosition.x - thumbnail.transform.position.x;
-                speed = Mathf.Lerp(speed, 0, 1 - xDistance);
+                currentSpeed = Mathf.Lerp(startSpeed, 0, 1 - xDistance);
             }
         }
     }
